ad694562103c8d2f0c649aa6ff9941855809679a
oujin <1354100681@qq.com>
2021-02-28 16:58:35 +0800

    update

---
+++ common_config.py
@@ -0,0 +1,22 @@
+# global server node
+server = {
+    'host': '192.168.1.143',
+    'port': '9527'
+}
+# local client nodes
+clients = [
+    {
+        'host': '192.168.1.144',
+        'port': '9527'
+    },
+    {
+        'host': '192.168.1.145',
+        'port': '9527'
+    },
+    {
+        'host': '192.168.1.146',
+        'port': '9527'
+    },
+]
+# cache dir to store the local models
+cache_dir = 'cache/'
